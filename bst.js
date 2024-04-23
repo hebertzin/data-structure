06254450f4295e8e34e806e77c10a524e03111d5
hebertsanto <hebertsantos0704@gmail.com>
2024-04-23 14:27:03 -0300

    binary search tree has been implemented

---
+++ bst.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearch {
+  constructor() {
+    this.root = null;
+  }
+
+  search(value) {
+    return this._searchHelper(this.root, value);
+  }
+
+  _searchHelper(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (node.value === value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this._searchHelper(node.left, value);
+    } else {
+      return this._searchHelper(node.right, value);
+    }
+  }
+}
+
+const tree = new BinarySearch();
+
+tree.root = new Node(20);
+tree.root.left = new Node(4);
+tree.root.right = new Node(15);
+tree.root.right.right = new Node(150);
+
+console.log(tree.search(150));
