0513a6983ca2b94b337f81f211e0fe673fc150ff
hebertsanto <hebertsantos0704@gmail.com>
2024-04-23 13:39:00 -0300

    solve linear search problem

---
+++ linear-search.js
@@ -0,0 +1,16 @@
+function linearSearch(array, target) {
+  const length = array.length - 1;
+
+  for (let i = 0; i <= length; i++) {
+    if (array[i] == target) {
+      return array[i];
+    }
+  }
+
+  return -1; // element was not found in the array
+}
+
+const listOfElements = [40, 20, 498, 29, 982, 82];
+
+const successExemple = linearSearch(listOfElements, 20); // output : 20
+const badExemple = linearSearch(listOfElements, 87); // element does not exist in the list
